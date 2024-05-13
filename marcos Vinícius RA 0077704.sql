4914ed60f13860b0f7d77b52821e05abe71f6c48
Vitalmoura <marcos.vital526@gmail.com>
2024-05-13 18:26:08 -0300

    Add files via upload

---
+++ marcos Vinícius RA 0077704.sql
@@ -0,0 +1,17 @@
+INSERT INTO livros (titulo, autor, ano_de_publicacao, disponivel, categoria, fantasia, ISBN, editora, paginas, idioma);
+
+UPDATE editora
+SET column 1 
+WHERE 1984 = plume books
+
+DELETE FROM frances WHERE 1970
+
+GROUP BY livros
+
+LIMIT livros = 5
+
+AVG = ano_de_publicacao/qtd
+
+ORDER BY livros
+FROM ordem decrescente
+
